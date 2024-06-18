a9606838931b91d9d4475fc93805ba89351c8652
gabrieldeori <deori.pessoal@gmail.com>
2024-06-18 16:35:35 -0300

    lint: fix lint errors with ruff

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,8 @@
-from fast_zero.app import app
-from fastapi.testclient import TestClient
 from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from fast_zero.app import app
 
 
 def test_read_root_must_return_ok_and_Hello_World():
