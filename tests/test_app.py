ccfe0744fbb239381c9bfe7f4bfb7dcbc3122110
gabrieldeori <deori.pessoal@gmail.com>
2024-06-27 14:54:15 -0300

    test: delete user

--- tests/test_app.py
+++ tests/test_app.py
@@ -60,3 +60,9 @@ def test_update_user(client):
         'email': 'test@email.com',
         'id': 1,
     }
+
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+
+    assert response.json() == {'message': 'User deleted'}
