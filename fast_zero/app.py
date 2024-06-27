cfed93ba2a3346d60570240eb142445e9a62c04f
gabrieldeori <deori.pessoal@gmail.com>
2024-06-27 11:26:44 -0300

    feat: fake DB

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -2,9 +2,11 @@ from http import HTTPStatus
 
 from fastapi import FastAPI
 
-from fast_zero.schemas import Message, UserSchema, UserPublic
+from fast_zero.schemas import Message, UserSchema, UserPublic, UserDB
 
 app = FastAPI()
+
+database = []
 
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
@@ -13,4 +15,14 @@ def read_root():
 
 @app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
 def create_user(user: UserSchema):
-    return user
+
+    # breakpoint() # Para a aplicação ('l' mostra a linha, 'q' sai do debug)
+
+    user_with_id = UserDB(
+        id=len(database) + 1,
+        **user.model_dump() # Transforma dado em dicionário, e o ** faz tipo o ... do js
+    )
+
+    database.append(user_with_id)
+
+    return user_with_id
--- fast_zero/schemas.py
+++ fast_zero/schemas.py
@@ -1,7 +1,8 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 class Message(BaseModel):
     message: str
+
 
 class UserSchema(BaseModel):
     username: str
@@ -9,5 +10,9 @@ class UserSchema(BaseModel):
     password: str
 
 class UserPublic(BaseModel):
+    id: int
     username: str
     email: EmailStr
+
+class UserDB(UserSchema):
+    id: int
