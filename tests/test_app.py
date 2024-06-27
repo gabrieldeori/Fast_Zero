703e196de5f27a8012cea693595ef8997a203402
gabrieldeori <deori.pessoal@gmail.com>
2024-06-27 15:11:25 -0300

    feat: test get user by id

--- tests/test_app.py
+++ tests/test_app.py
@@ -44,6 +44,17 @@ def test_read_users(client):
     }
 
 
+def test_read_user(client):
+    response = client.get('/users/1')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'id': 1,
+        'username': 'testusername',
+        'email': 'test@email.com',
+    }
+
+
 def test_update_user(client):
     response = client.put(
         '/users/1',
