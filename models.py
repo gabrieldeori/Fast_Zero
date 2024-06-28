0eb63ebbdb452ccacc94ea38a608f2926b526838
gabrieldeori <deori.pessoal@gmail.com>
2024-06-28 11:44:03 -0300

    feat: user model

---
+++ models.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+
+from sqlalchemy import func
+from sqlalchemy.orm import registry, Mapped, mapped_column
+
+table_registry = registry()
+
+@table_registry.mapped_as_dataclass
+class User:
+    __tablename__ = 'users'
+
+    id: Mapped[int] = mapped_column(init=False, primary_key=True)
+    # Init é pra dizer que não precisa pra inicializar no banco
+    # O SQLAlchemy não atribui fica none
+    # E o banco que vai atribuir essa coluna ;)
+    username: Mapped[str] = mapped_column(unique=True)
+    password: Mapped[str]
+    email: Mapped[str] = mapped_column(unique=True)
+    created_at: Mapped[datetime] = mapped_column(init=False, server_default=func.now())
