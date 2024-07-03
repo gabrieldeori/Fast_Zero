7be0ccaec75a2beea7141e6bb25e02e5cb99daf5
gabrieldeori <deori.pessoal@gmail.com>
2024-07-03 13:29:13 -0300

    test: test create user on DB

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,8 +1,43 @@
-from fast_zero.models import User
+from sqlalchemy import create_engine, select
+from sqlalchemy.orm import Session
+
+from fast_zero.models import User, table_registry
+
+TEST_USERNAME = 'test'
+TEST_EMAIL = 'test@test.com'
+TEST_PASSWORD = 'P@55w0rd'
+
+
+def test_class_create_user():
+    user = User(
+        username=TEST_USERNAME, email=TEST_EMAIL, password=TEST_PASSWORD
+    )
+
+    assert user.username == TEST_USERNAME
+    assert user.email == TEST_EMAIL
+    assert user.password == TEST_PASSWORD
+
 
 def test_create_user():
-    user = User(username='test', email='test@test.com', password='secret')
+    # engine = create_engine('sqlite:///database.db')
+    engine = create_engine('sqlite:///:memory:')
+    # Dura apenas durante o teste
 
-    assert user.username == 'test'
-    assert user.email == 'test@test.com'
-    assert user.password == 'secret'
+    table_registry.metadata.create_all(engine)
+
+    with Session(engine) as session:
+        user = User(
+            username=TEST_USERNAME, email=TEST_EMAIL, password=TEST_PASSWORD
+        )
+
+        session.add(user)
+        session.commit()
+        # session.refresh(user)
+
+        result = session.scalar(
+            select(User).where(User.email == TEST_EMAIL)
+        )
+        # Query Mapeia e retorna resultado como objeto
+
+    # assert user.id == 1
+    assert result.username == TEST_USERNAME
