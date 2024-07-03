ed7e305e6a85caa851495262ce1d38003f1f797d
gabrieldeori <deori.pessoal@gmail.com>
2024-07-03 15:22:22 -0300

    feat: improve with database

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,14 +1,11 @@
 from http import HTTPStatus
 
 from fastapi import FastAPI, HTTPException
+from sqlalchemy import select
 
+from fast_zero.database import get_session
 from fast_zero.models import User
 from fast_zero.schemas import Message, UserDB, UserList, UserPublic, UserSchema
-from fast_zero.settings import Settings
-
-from sqlalchemy import create_engine, select
-from sqlalchemy.orm import Session
-
 
 app = FastAPI()
 
@@ -23,34 +20,35 @@ def read_root():
 @app.get
 @app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
 def create_user(user: UserSchema):
-    engine = create_engine(Settings().DATABASE_URL)
+    session = get_session()
 
-    with Session(engine) as session:
-        db_user = session.scalar(
-            select(User).where(
-                (User.username == user.username) | (User.email == user.email)
-            )
+    db_user = session.scalar(
+        select(User).where(
+            (User.username == user.username) | (User.email == user.email)
         )
+    )
 
-        if db_user:
-            if db_user.username == user.username:
-                raise HTTPException(
-                    status_code=HTTPStatus.BAD_REQUEST,
-                    detail='Username already exists'
-                )
-            elif db_user.email == user.email:
-                raise HTTPException(
-                    status_code=HTTPStatus.BAD_REQUEST,
-                    detail='Email already exists'
-                )
-        
-        db_user = User(username=user.username, email=user.email, password=user.password)
+    if db_user:
+        if db_user.username == user.username:
+            raise HTTPException(
+                status_code=HTTPStatus.BAD_REQUEST,
+                detail='Username already exists',
+            )
+        elif db_user.email == user.email:
+            raise HTTPException(
+                status_code=HTTPStatus.BAD_REQUEST,
+                detail='Email already exists',
+            )
 
-        session.add(db_user)
-        session.commit()
-        session.refresh(db_user)
+    db_user = User(
+        username=user.username, email=user.email, password=user.password
+    )
 
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
     return db_user
+
 
 @app.get('/users/', status_code=HTTPStatus.OK, response_model=UserList)
 def read_users():
