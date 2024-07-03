f66d9d062d6782a05be95a68ea8b78d130bc4ea9
gabrieldeori <deori.pessoal@gmail.com>
2024-07-03 16:52:03 -0300

    refact: check thread and pool

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,13 @@
 import pytest
 from fastapi.testclient import TestClient
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
 
 from fast_zero.app import app
 from fast_zero.database import get_session
 from fast_zero.models import table_registry
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from sqlalchemy.pool import StaticPool
 
 @pytest.fixture()
 def client(session):
@@ -24,7 +25,13 @@ def client(session):
 @pytest.fixture()
 def session():
 
-    engine = create_engine('sqlite:///:memory:')
+    engine = create_engine(
+        'sqlite:///:memory:',
+        connect_args={
+            'check_same_thread': False,
+        },
+        poolclass=StaticPool,
+    )
 
     table_registry.metadata.create_all(engine)
 
